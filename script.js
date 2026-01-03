0500fae8c6a42e439bb7a3a968cc9b960eb0cfbd
next-one-grp <it.nextone@gmail.com>
2026-01-03 17:22:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Stats fictives (modifiable via API Discord plus tard)
+document.getElementById("members").textContent = 1240;
+document.getElementById("active").textContent = 215;
+document.getElementById("services").textContent = 9;
+document.getElementById("staff").textContent = 6;
+
